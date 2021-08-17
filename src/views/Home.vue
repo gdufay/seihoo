5c9910621caf6cdbf14e3820d2f0a2d4b036e543
gdufay <gdufay@student.42.fr>
2021-08-17 10:25:43 +0200

    Basic template for Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,77 @@
 <template>
-  <h1>Hello World!</h1>
+  <el-card class="column-card">
+    <template #header>
+      <div class="card-header">
+        <span>Recipes</span>
+      </div>
+    </template>
+  </el-card>
+
+  <el-card class="column-card">
+    <template #header>
+      <div class="card-header">
+        <span>Ingredients</span>
+      </div>
+    </template>
+    <el-tag
+      v-for="ingredient in ingredients"
+      :key="ingredient"
+      closable
+      @close="removeIngredient(ingredient)"
+    >
+      {{ ingredient.name }}
+    </el-tag>
+  </el-card>
+
+  <el-card class="column-card">
+    <template #header>
+      <div class="card-header">
+        <span>Selected recipes</span>
+      </div>
+    </template>
+  </el-card>
+
+  <el-card class="column-card">
+    <template #header>
+      <div class="card-header">
+        <span>Shopping List</span>
+      </div>
+    </template>
+  </el-card>
 </template>
 
 <script>
 export default {
-    name: "Home",
-}
+  name: "Home",
+
+  data() {
+    return {
+      ingredients: [{ name: "water" }, { name: "milk" }],
+    };
+  },
+
+  mounted() {
+    /*
+    const query = new this.$Parse.Query("Ingredient");
+    query
+      .select("name")
+      .find()
+      .then((results) => {
+        console.log(`ParseObjects found: ${JSON.stringify(results)}`);
+      })
+      .catch((error) => {
+        console.log(`Error: ${JSON.stringify(error)}`);
+      });
+      */
+  },
+
+  methods: {
+    removeIngredient(ingredient) {
+      console.log("Removing ingredient", ingredient);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
