1ad1f7790f68edfddf4e72e1e0e48fac4e731aca
gdufay <gdufay@student.42.fr>
2021-08-17 13:45:23 +0200

    Proper component for ingredient list

---
+++ src/components/IngredientList.vue
@@ -0,0 +1,116 @@
+<template>
+  <el-card>
+    <template #header>
+      <div class="card-header">
+        <span>Ingredients</span>
+      </div>
+    </template>
+    <el-tag
+      v-for="ingredient in ingredients"
+      :key="ingredient"
+      closable
+      @close="removeIngredient(ingredient)"
+    >
+      {{ ingredient.get("name") }}
+    </el-tag>
+
+    <el-input
+      class="input-new-tag"
+      v-if="inputVisible"
+      v-model="inputValue"
+      ref="saveTagInput"
+      size="mini"
+      @keyup.enter="handleInputConfirm"
+      @blur="handleInputConfirm"
+    >
+    </el-input>
+    <el-button v-else class="button-new-tag" size="small" @click="showInput"
+      >+ New Ingredient
+    </el-button>
+  </el-card>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "ingredient-list",
+
+  data() {
+    return {
+      inputVisible: false,
+      inputValue: "",
+    };
+  },
+
+  computed: {
+    ...mapState({
+      ingredients: (state) => state.ingredients.ingredients,
+    }),
+  },
+
+  mounted() {
+    this.$store.dispatch("getAllIngredients");
+  },
+
+  methods: {
+    removeIngredient(ingredient) {
+      this.$confirm(
+        "This will permanently delete the ingredient. Continue?",
+        "Warning",
+        {
+          confirmButtonText: "OK",
+          cancelButtonText: "Cancel",
+          type: "warning",
+        }
+      )
+        .then(() => this.$store.dispatch("removeIngredient", ingredient))
+        .then(() =>
+          this.$message({ type: "success", message: "Delete completed" })
+        )
+        .catch((e) =>
+          this.$message(
+            e === "cancel"
+              ? { type: "info", message: "Delete canceled" }
+              : { type: "error", message: e.message }
+          )
+        );
+    },
+
+    showInput() {
+      this.inputVisible = true;
+      this.$nextTick(() => {
+        this.$refs.saveTagInput.$refs.input.focus();
+      });
+    },
+
+    handleInputConfirm() {
+      const ingredientName = this.inputValue.trim();
+
+      if (ingredientName) {
+        this.$store.dispatch("addIngredient", ingredientName);
+      }
+      this.inputVisible = false;
+      this.inputValue = "";
+    },
+  },
+};
+</script>
+
+<style>
+.el-tag + .el-tag {
+  margin-left: 10px;
+}
+.button-new-tag {
+  margin-left: 10px;
+  height: 32px;
+  line-height: 30px;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+.input-new-tag {
+  width: 90px;
+  margin-left: 10px;
+  vertical-align: bottom;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,35 +7,7 @@
     </template>
   </el-card>
 
-  <el-card class="column-card">
-    <template #header>
-      <div class="card-header">
-        <span>Ingredients</span>
-      </div>
-    </template>
-    <el-tag
-      v-for="ingredient in ingredients"
-      :key="ingredient"
-      closable
-      @close="removeIngredient(ingredient)"
-    >
-      {{ ingredient.get("name") }}
-    </el-tag>
-
-    <el-input
-      class="input-new-tag"
-      v-if="inputVisible"
-      v-model="inputValue"
-      ref="saveTagInput"
-      size="mini"
-      @keyup.enter="handleInputConfirm"
-      @blur="handleInputConfirm"
-    >
-    </el-input>
-    <el-button v-else class="button-new-tag" size="small" @click="showInput"
-      >+ New Ingredient</el-button
-    >
-  </el-card>
+  <ingredient-list class="column-card"></ingredient-list>
 
   <el-card class="column-card">
     <template #header>
@@ -55,86 +27,27 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
+import IngredientList from '../components/IngredientList.vue';
 
 export default {
   name: "Home",
 
+  components: {
+    IngredientList,
+  },
+
   data() {
-    return {
-      inputVisible: false,
-      inputValue: "",
-    };
+    return {};
   },
 
-  computed: {
-    ...mapState({
-      ingredients: (state) => state.ingredients.ingredients,
-    }),
-  },
+  computed: {},
 
-  mounted() {
-    this.$store.dispatch("getAllIngredients");
-  },
+  mounted() {},
 
-  methods: {
-    removeIngredient(ingredient) {
-      this.$confirm(
-        "This will permanently delete the ingredient. Continue?",
-        "Warning",
-        {
-          confirmButtonText: "OK",
-          cancelButtonText: "Cancel",
-          type: "warning",
-        }
-      )
-        .then(() => this.$store.dispatch("removeIngredient", ingredient))
-        .then(() =>
-          this.$message({ type: "success", message: "Delete completed" })
-        )
-        .catch((e) =>
-          this.$message(
-            e === "cancel"
-              ? { type: "info", message: "Delete canceled" }
-              : { type: "error", message: e.message }
-          )
-        );
-    },
-
-    showInput() {
-      this.inputVisible = true;
-      this.$nextTick(() => {
-        this.$refs.saveTagInput.$refs.input.focus();
-      });
-    },
-
-    handleInputConfirm() {
-      const ingredientName = this.inputValue.trim();
-
-      if (ingredientName) {
-        this.$store.dispatch("addIngredient", ingredientName);
-      }
-      this.inputVisible = false;
-      this.inputValue = "";
-    },
-  },
+  methods: {},
 };
 </script>
 
 <style>
-.el-tag + .el-tag {
-  margin-left: 10px;
-}
-.button-new-tag {
-  margin-left: 10px;
-  height: 32px;
-  line-height: 30px;
-  padding-top: 0;
-  padding-bottom: 0;
-}
-.input-new-tag {
-  width: 90px;
-  margin-left: 10px;
-  vertical-align: bottom;
-}
 </style>
+    IngredientList
