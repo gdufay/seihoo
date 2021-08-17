9ab97aee6503182d36b6c6de5a67749fa4a2afe9
gdufay <gdufay@student.42.fr>
2021-08-17 10:25:54 +0200

    Add some explanations

--- src/main.js
+++ src/main.js
@@ -8,7 +8,8 @@ const app = createApp(App).use(router)
 installElementPlus(app)
 app.mount('#app')
 
+// Pare initialization
 Parse.initialize("0PpmnebENvw8ccfGRSqesLXGVGsRMJOpEvZz2Hei", "BX6kliENeqAtsDZe6r5Wxwud6qax5THTcmyp5KkB");
 Parse.serverURL = "https://parseapi.back4app.com/";
-
+// Make Parse accessible with this
 app.config.globalProperties.$Parse = Parse;
