b24026118e8c9934f25a696080c0a2569e00641f
gdufay <gdufay@student.42.fr>
2021-08-19 23:07:19 +0200

    Add RecipeItem component

---
+++ src/components/RecipeItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-card class="recipe-item">
+    <template #header>
+      <div class="card-header">
+        {{ recipe.get("name") }}
+      </div>
+    </template>
+
+    <div class="ingredients-container">
+      <ingredient-item
+        v-for="{ ingredient, quantity } in recipe.get('ingredients')"
+        :key="ingredient.get('name')"
+        :name="ingredient.get('name')"
+        :quantity="quantity"
+        :unit="ingredient.get('unit').get('name')"
+      ></ingredient-item>
+    </div>
+
+    <slot name="footer" :item="recipe"></slot>
+  </el-card>
+</template>
+
+<script>
+import IngredientItem from "./IngredientItem.vue";
+export default {
+  name: "recipe-item",
+
+  props: {
+    recipe: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  components: { IngredientItem },
+};
+</script>
+
+<style>
+</style>
--- src/components/RecipeList.vue
+++ src/components/RecipeList.vue
@@ -6,39 +6,28 @@
       </div>
     </template>
 
-    <el-card v-for="recipe in recipes" :key="recipe.get('name')">
-      <template #header>
-        <div class="card-header">
-          {{ recipe.get("name") }}
-        </div>
+    <recipe-item
+      v-for="recipe in recipes"
+      :key="recipe.get('name')"
+      :recipe="recipe"
+    >
+      <template #footer="{ item }">
+        <el-button @click="selectRecipe(item)">Select</el-button>
+        <el-button @click="editRecipe(item)">Edit</el-button>
+        <el-button @click="removeRecipe(item)">Remove</el-button>
       </template>
-
-      <div class="ingredients-container">
-        <ingredient-item
-          v-for="{ ingredient, quantity } in recipe.get('ingredients')"
-          :key="ingredient.get('name')"
-          :name="ingredient.get('name')"
-          :quantity="quantity"
-          :unit="ingredient.get('unit').get('name')"
-        ></ingredient-item>
-      </div>
-
-      <el-button @click="editRecipe(recipe)">Edit</el-button>
-      <el-button @click="removeRecipe(recipe)">Remove</el-button>
-    </el-card>
+    </recipe-item>
   </el-card>
 </template>
 
 <script>
 import { mapState } from "vuex";
-import IngredientItem from "./IngredientItem.vue";
+import RecipeItem from "./RecipeItem.vue";
 
 export default {
   name: "recipe-list",
 
-  components: {
-    IngredientItem,
-  },
+  components: { RecipeItem },
 
   computed: {
     ...mapState({
@@ -53,6 +42,10 @@ export default {
 
     removeRecipe(recipe) {
       console.log("Removing recipe:", recipe);
+    },
+
+    selectRecipe(recipe) {
+      console.log("Selection recipe:", recipe);
     },
   },
 };
