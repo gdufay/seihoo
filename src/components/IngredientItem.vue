1e27e256abae5a8c5a1987151f41d599d066ab8e
gdufay <gdufay@student.42.fr>
2021-08-19 22:21:00 +0200

    Create IngredientItem component

---
+++ src/components/IngredientItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="ingredient-item">
+    <span class="ingredient-name">{{ name }}</span>
+    <span class="ingredient-measurement">{{ quantity }} {{ unit }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ingredient-item",
+
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    unit: String,
+  },
+};
+</script>
+
+<style>
+.ingredient-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.25rem;
+}
+</style>
