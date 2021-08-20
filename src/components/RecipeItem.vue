67b775f6b69f4f5d680407d17225564120409db4
gdufay <gdufay@student.42.fr>
2021-08-20 20:00:22 +0200

    Update css and better scalability

--- src/components/RecipeItem.vue
+++ src/components/RecipeItem.vue
@@ -1,10 +1,22 @@
 <template>
   <el-card class="recipe-item">
-    <template #header>
-      <div class="card-header">
-        {{ recipe.get("name") }}
-      </div>
-    </template>
+    <div class="card-header item-name">
+      <span>{{ recipe.get("name") }}</span>
+
+      <el-dropdown v-if="actions" trigger="click" @command="handleCommand">
+        <el-icon class="dropdown-link" :size="24">
+          <more-filled :style="iconStyle" />
+        </el-icon>
+
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item v-for="action in actions" :key="action.id" :command="action.id">
+              {{ action.text }}
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
 
     <div class="ingredients-container">
       <ingredient-item
@@ -12,16 +24,16 @@
         :key="ingredient.get('name')"
         :name="ingredient.get('name')"
         :quantity="quantity"
-        :unit="ingredient.get('unit').get('name')"
+        :unit="unitNameOrNothing(ingredient)"
       ></ingredient-item>
     </div>
-
-    <slot name="footer" :item="recipe"></slot>
   </el-card>
 </template>
 
 <script>
 import IngredientItem from "./IngredientItem.vue";
+import { MoreFilled } from "@element-plus/icons";
+
 export default {
   name: "recipe-item",
 
@@ -30,11 +42,55 @@ export default {
       type: Object,
       required: true,
     },
+    actions: Array,
   },
 
-  components: { IngredientItem },
+  components: { IngredientItem, MoreFilled },
+
+  emits: ["menuAction"],
+
+  data() {
+    return {
+      iconStyle: {
+        width: "1.5rem",
+        height: "1.5rem",
+      },
+    };
+  },
+
+  methods: {
+    // TODO: quickfixe, change
+    unitNameOrNothing(ingredient) {
+      let name = "";
+
+      try {
+        name = ingredient.get("unit").get("name");
+      } catch (e) {
+        console.error(e.message);
+      }
+
+      return name;
+    },
+
+    handleCommand(command) {
+      this.$emit("menuAction", { action: command, recipe: this.recipe });
+    },
+  },
 };
 </script>
 
 <style>
+.recipe-item {
+  margin-bottom: 0.5rem;
+}
+
+.item-name {
+  word-wrap: break-word;
+  clear: both;
+  color: #172b4d;
+  display: block;
+  margin: 0 0 4px;
+  overflow: hidden;
+  text-decoration: none;
+}
 </style>
--- src/components/RecipeList.vue
+++ src/components/RecipeList.vue
@@ -1,22 +1,24 @@
 <template>
   <el-card>
-    <template #header>
-      <div class="card-header">
-        <span>Recipes</span>
-      </div>
-    </template>
+    <div class="card-header column-header">
+      <span>Recipes</span>
+    </div>
 
-    <recipe-item
-      v-for="recipe in recipes"
-      :key="recipe.get('name')"
-      :recipe="recipe"
-    >
-      <template #footer="{ item }">
-        <el-button @click="selectRecipe(item)">Select</el-button>
-        <el-button @click="editRecipe(item)">Edit</el-button>
-        <el-button @click="removeRecipe(item)">Remove</el-button>
-      </template>
-    </recipe-item>
+    <el-scrollbar>
+      <recipe-item
+        v-for="recipe in recipes"
+        :key="recipe.get('name')"
+        :recipe="recipe"
+        :actions="actions"
+        @menuAction="handleMenuAction"
+      >
+        <template #dropdownMenu="{ item }">
+          <el-button @click="selectRecipe(item)">Select</el-button>
+          <el-button @click="editRecipe(item)">Edit</el-button>
+          <el-button @click="removeRecipe(item)">Remove</el-button>
+        </template>
+      </recipe-item>
+    </el-scrollbar>
   </el-card>
 </template>
 
@@ -24,10 +26,24 @@
 import { mapState } from "vuex";
 import RecipeItem from "./RecipeItem.vue";
 
+const SELECT_ACTION = "select";
+const EDIT_ACTION = "edit";
+const REMOVE_ACTION = "remove";
+
 export default {
   name: "recipe-list",
 
   components: { RecipeItem },
+
+  data() {
+    return {
+      actions: [
+        { id: SELECT_ACTION, text: "Select" },
+        { id: EDIT_ACTION, text: "Edit" },
+        { id: REMOVE_ACTION, text: "Remove" },
+      ],
+    };
+  },
 
   computed: {
     ...mapState({
@@ -36,6 +52,22 @@ export default {
   },
 
   methods: {
+    handleMenuAction({ action, recipe }) {
+      switch (action) {
+        case SELECT_ACTION:
+          this.selectRecipe(recipe);
+          break;
+        case EDIT_ACTION:
+          this.editRecipe(recipe);
+          break;
+        case REMOVE_ACTION:
+          this.removeRecipe(recipe);
+          break;
+        default:
+          console.error("Unknown action", action);
+      }
+    },
+
     editRecipe(recipe) {
       this.$router.push("/edit/" + recipe.id);
     },
