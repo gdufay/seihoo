32f2e3934ee354ba826bebe3c76a46fe19b9225d
gdufay <gdufay@student.42.fr>
2021-09-02 15:25:56 +0200

    Add custom icon

---
+++ src/components/icons/IconFood.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    version="1.1"
+    x="0px"
+    y="0px"
+    viewBox="0 0 100 100"
+    style="enable-background: new 0 0 100 100"
+    xml:space="preserve"
+  >
+    <path
+      d="M11.1,67v3h77.8v-3H11.1z M83.3,59.8C82.5,42,68.6,28,51.5,27.2v-5.5h-3v5.5C31.4,28,17.5,42,16.7,59.8l-0.1,1.6h66.7  L83.3,59.8z M19.8,58.4C21.3,42.4,34.3,30.2,50,30.2s28.7,12.3,30.2,28.2H19.8z"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "icon-food",
+};
+</script>
---
+++ src/components/icons/index.js
@@ -0,0 +1,24 @@
+import IconCereal from "./IconCereal.vue";
+import IconDairy from "./IconDairy.vue";
+import IconFish from "./IconFish.vue";
+import IconSeasoning from "./IconSeasoning.vue";
+import IconVegetable from "./IconVegetable.vue";
+import IconCandy from "./IconCandy.vue";
+import IconFood from "./IconFood.vue";
+import IconFruit from "./IconFruit.vue";
+import IconLiquid from "./IconLiquid.vue";
+import IconMeat from "./IconMeat.vue";
+
+
+export {
+    IconCereal,
+    IconDairy,
+    IconFish,
+    IconSeasoning,
+    IconVegetable,
+    IconCandy,
+    IconFood,
+    IconFruit,
+    IconLiquid,
+    IconMeat,
+}
