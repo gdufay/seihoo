960b2b2e10357736366c7adf8bdb543a9e1c5ba3
gdufay <gdufay@student.42.fr>
2021-09-07 17:58:31 +0200

    Axios and vuex-orm-axios plugin use

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,20 @@ import { Ingredient, Recipe, IngredientRecipe, Unit } from "../models";
 import ingredients from "./modules/ingredients";
 import recipes from './modules/recipes';
 import FetchWrapper from '../utils/FetchWrapper';
+import VuexOrmAxios from "@vuex-orm/plugin-axios";
+import axios from 'axios';
 
 const debug = process.env.NODE_ENV !== "production";
+
+VuexORM.use(VuexOrmAxios, {
+  axios,
+  headers: {
+    "Content-Type": "application/json",
+    "X-Parse-Application-Id": "0PpmnebENvw8ccfGRSqesLXGVGsRMJOpEvZz2Hei",
+    "X-Parse-REST-API-Key": "Ck76d2h5GmkJSpxB7U26sQyyV6UHZV7qtRxYR2Sg",
+  },
+  baseURL: "https://parseapi.back4app.com/",
+});
 
 const database = new VuexORM.Database();
 
@@ -31,9 +43,9 @@ export default createStore({
     },
 
     async getAllUnits() {
-        return new FetchWrapper("https://parseapi.back4app.com/classes/Unit")
-            .fetch()
-            .then(({ results }) => Unit.insert({ data: results }));
+      return new FetchWrapper("https://parseapi.back4app.com/classes/Unit")
+        .fetch()
+        .then(({ results }) => Unit.insert({ data: results }));
     }
   },
 
